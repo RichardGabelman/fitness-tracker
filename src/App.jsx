7611174c1ce5e911c9f446f5fcf83665e6fdd8c3
Richard Gabelman <78068360+RichardGabelman@users.noreply.github.com>
2025-05-04 05:28:35 -0700

    Add basic design

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Icon from '@mdi/react';
+import { mdiHeartPulse, mdiRun } from '@mdi/js';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [steps, setSteps] = useState(0);
+  const [heartrate, setHeartrate] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>CS578 Fitness Tracker</header>
+      <main>
+        <section>
+          <article>
+            <Icon path={mdiRun} size={6} color="green"/>
+            <h2>Steps: {steps}</h2>
+          </article>
+          <article>
+            <Icon path={mdiHeartPulse} size={6} color="red"/>
+            <h2>Heart Rate: {heartrate}</h2>
+          </article>
+        </section>
+      </main>
+      <footer>Copyright &copy; {new Date().getFullYear()} CS578 Group 2</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
